8f3caf4d2cca8085f9eea570d8df6835a8f8eacf
Emma Herbolzheimer <emmajherbolzheimer@gmail.com>
2016-10-31 17:26:14 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,19 +30,91 @@ def move(position,token="X")
   @board[position-1] = token
 end
 
-def position_taken?(index)
-  if @board[index] == " " || @board[index] == "" || @board[index] ==nil
+def position_taken?(position)
+  if @board[position] == " " || @board[position] == "" || @board[position] ==nil
     false
   else
     true
   end
 end
 
-def valid_move?(index)
-  index=index.to_i
-  if !position_taken?(index) && index.between?(0,8)
+def valid_move?(position)
+  position=position.to_i-1
+  if position.between?(0,8) && !position_taken?(position)
     true
   end
 end
+
+def turn
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  position= user_input.to_i
+  if valid_move?(position)
+      move(position, current_player)
+    else turn
+  end
+  display_board
+end
+
+def turn_count
+  counter = 0
+  @board.collect do |board|
+    if board == "X" || board == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player
+  if turn_count%2 == 0
+    "X"
+  else
+    "O"
+  end
+end
+
+def won?
+  WIN_COMBINATIONS.detect do |win_index|
+    @board[win_index[0]] == @board[win_index[1]] && @board[win_index[1]] == @board[win_index[2]] && position_taken?(win_index[0])
+  end
+end
+
+def full?
+  @board.all? do |board_index|
+    board_index == "X" || board_index == "O"
+  end
+end
+
+def draw?
+  if full? && !won?
+   true
+  end
+end
+
+def over?
+  won? || draw?
+end
+
+def winner
+won?
+if won? != nil
+@board[won?[0]]
+    end
+  end
+
+  def play
+
+    until over?
+    turn
+    end
+
+    if draw?
+    puts "Cats Game!"
+    else won?
+      puts "Congratulations #{winner}!"
+    end
+  end
+
 
 end
