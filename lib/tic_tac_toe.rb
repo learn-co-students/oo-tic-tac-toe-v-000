0c6dbe5bf8c173cfa8b8055c353964d31b984c31
Tayari Appiah <cjhhh.ta@gmail.com>
2016-10-31 23:58:37 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,12 +42,12 @@ class TicTacToe
     @board[index.to_i - 1] = token
   end
 
-  def position_taken?(board, index)
+  def position_taken?(index)
     @board[index] == "X" || @board[index] == "O"
   end
 
   def valid_move?(index)
-    index.between?(0,8)  && !position_taken?(board, index)
+    index.to_i.between?(1,9)  && !position_taken?(index.to_i - 1)
   end
 
 
