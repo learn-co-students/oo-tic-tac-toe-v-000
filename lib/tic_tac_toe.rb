11b250e1668dc77026c75da3b34fd323326a29bb
Emily Sugano <emily.sugano@gmail.com>
2017-03-14 16:44:28 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,27 +58,10 @@ end
     end
   end
 
-#def turn_count(board)
-#    count = 0
-#    @board.each do |index|
-#      if index != " "
-#        count += 1
-#      end
-#    end
-#    return count
-#  end
-
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
   end
 
-#  def current_player(board)
-#    if turn_count(board) % 2 == 0
-#      return "X"
-#    else
-#      return "O"
-#    end
-#  end
 def current_player
    turn_count % 2 == 0 ? "X" : "O"
  end
