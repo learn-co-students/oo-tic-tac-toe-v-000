5aeb902b46053877c5239b56f7cac26b6725c02e
Brian Zoebisch <zoebisch@gmail.com>
2017-04-20 03:41:07 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,6 @@
-require "pry"
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
 class TicTacToe
-  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
   def initialize(index = nil, board = nil)
     puts "Welcome to Tic Tac Toe!"
