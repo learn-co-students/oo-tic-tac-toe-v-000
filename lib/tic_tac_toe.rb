43e83c843ec664f35a50c87786c2482ab269dce6
Veronica Miller <vmcmiller@gmail.com>
2017-06-25 18:39:55 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,108 @@
+class TicTacToe
+
+  def initialize(board = nil)
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def move(index, player = "X")
+    @board[index] = player
+  end
+
+  def position_taken?(index)
+    !(@board[index] == " " || @board[index] == "" || @board[index] == nil)
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+
+  def turn_count
+    turns = 0
+    @board.each do |location|
+      if (location == "X" || location == "O")
+        turns += 1
+      end
+    end
+    turns
+  end
+
+  def current_player
+    turn_count.even? ? "X" : "O"
+  end
+
+  def won?
+    won = nil
+    WIN_COMBINATIONS.each do |win_combo|
+      row = []
+      win_combo.each do |position|
+        if position_taken?(position)
+          row << @board[position]
+        end
+      end
+      if (row.length == 3 && (!row.include?("X") || !row.include?("O")))
+        won = win_combo
+      end
+    end
+    won
+  end
+
+  def full?
+    @board.all? do |position|
+      position == "X" || position == "O"
+    end
+  end
+
+  def draw?
+    full? && !won?
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def winner
+    if won?
+      @board[won?[0]]
+    else
+      won?
+    end
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    index = input_to_index(gets.strip)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+
+  def play
+    until over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    end
+    if draw?
+      puts "Cat's Game!"
+    end
+  end
+
+end
