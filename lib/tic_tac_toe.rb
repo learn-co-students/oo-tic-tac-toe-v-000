6608d84224349c3bdb40761afdf8ac76b0ebdcb0
Sara Schwarz <saramschwarz@gmail.com>
2017-11-03 02:17:05 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,6 +11,14 @@ def board
   @board
 end
 
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -22,13 +30,20 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def input_to_index(user_input)
+   index = user_input.to_i - 1
 end
+
+def move(index, char)
+@board[index] = char
+end
+
+def position_taken?(board, index)
+taken = nil
+if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+taken = false
+else
+taken = true
+end
+
 end
