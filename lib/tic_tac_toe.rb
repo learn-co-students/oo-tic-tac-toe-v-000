10673ff52c4bc7a9eb0aa7ca179ef6b54e7e7821
Sara Schwarz <saramschwarz@gmail.com>
2017-11-03 20:13:43 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,12 +38,46 @@ def move(index, char)
 @board[index] = char
 end
 
-def position_taken?(board, index)
-taken = nil
-if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-taken = false
-else
-taken = true
+def current_player
+  turn_count.even? ? "X" : "O"
 end
 
+def turn
+  puts "Please enter 1-9:"
+  i = gets.strip
+  index = input_to_index(i)
+  m = valid_move?(index)
+  if m == true
+    move(index, current_player)
+  else m == false
+    until m == true
+      puts "Sorry, that was an invalid move. Please enter 1-9:"
+      display_board(@board)
+      i = gets.strip
+      index = input_to_index(i)
+      m = valid_move?(index)
+      move(index, current_player)
+    end
+  end
+end
+def turn_count
+  count = 0
+  board.each do |move|
+    if move == "X" || move == "O"
+      count += 1
+    end
+  end
+  count
+end
+
+def position_taken?(index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(index)
+if !position_taken?(index) && index.between?(0, 8)
+  true
+else false
+end
+end
 end
