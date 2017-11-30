72e9dd5931008f4639fa307a14fbcf7ba68de10b
Reno Sorensen <renojames19@gmail.com>
2017-11-30 02:24:00 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,133 @@
+class TicTacToe
+
+  def initialize
+    @board = Array.new(9, " ")
+  end
+
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+  ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def move(index, value)
+    @board[index] = value
+  end
+
+  def position_taken?(location)
+    @board[location] != " " && @board[location] != ""
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+
+  def turn_count
+    turn = 0
+    @board.each do |position|
+      if position == "X" || position == "O"
+        turn += 1
+      end
+    end
+    return turn
+  end
+
+  def current_player
+    player = turn_count
+    if player.even?
+      return "X"
+    else
+      return "O"
+    end
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    value = current_player
+    if !valid_move?(index)
+      user_input = gets.strip
+    else move(index, value)
+    end
+    display_board
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+
+      position_1 = @board[win_index_1]
+      position_2 = @board[win_index_2]
+      position_3 = @board[win_index_3]
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return win_combination
+      end
+    end
+    return false
+  end
+
+  def full?
+    @board.none? do |index|
+      index == " "
+    end
+  end
+
+  def draw?
+    if !won? && full?
+      return true
+    else
+      return false
+    end
+  end
+
+  def over?
+    if won? || draw? || full?
+      return true
+    else
+      return false
+    end
+  end
+
+  def winner
+    result = won?
+    if result
+      if @board[result[0]] == "X"
+        return "X"
+      else
+        return "O"
+      end
+    end
+  end
+
+  def play
+    until over?
+      turn
+    end
+    if draw?
+      puts "Cat's Game!"
+    else
+      puts "Congratulations #{winner}!"
+    end
+  end
+
+end
