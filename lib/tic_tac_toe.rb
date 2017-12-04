b3cd4c1de85e1319407fd681b96736532409c910
neodynia <neodynia@icloud.com>
2017-12-04 00:58:55 -0500

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,5 +14,42 @@ class TicTacToe
     [2,4,6]
   ]
 
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    "#{input}".to_i - 1
+  end
+
+  def move(index,token="X")
+    @board[index] = token
+  end
+
+  def position_taken?(index)
+    if @board[index] == "X" || @board[index] == "O"
+      return true
+    else
+      return false
+    end
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+
+  def turn_count
+    @board.count do |i|
+      i == "X" || i == "O"
+    end
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
 
 end
