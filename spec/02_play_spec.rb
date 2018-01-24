817bd50a1d4370905116676a721988f12fded05e
Jeamay Palo <jeamay.palo@gmail.com>
2018-01-24 04:35:07 +0000

    Done.

--- spec/02_play_spec.rb
+++ spec/02_play_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative '../lib/tic_tac_toe.rb'
 
 describe './lib/tic_tac_toe.rb' do
@@ -5,6 +6,7 @@ describe './lib/tic_tac_toe.rb' do
     describe '#play' do
       it 'asks for players input on a turn of the game' do
         game = TicTacToe.new
+        binding.pry
         allow($stdout).to receive(:puts)
         allow(game).to receive(:over?).and_return(false, true)
 
