d05b9557db02e63007a25b45fc5e9201fec68b5d
Andrew Hong <hongandrew16@gmail.com>
2018-02-08 04:24:47 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,94 +31,81 @@ class TicTacToe
   end
 
   def position_taken?(index)
-    @board[index] != " "
+    @board[index] == "X" || @board[index] == "O"
   end
 
   def valid_move?(index)
-    index.between?(0,8) && position_taken?(index) == false
+    !position_taken?(index) && index.between?(0,8)
   end
 
-
   def turn_count
-    turns_played = 0
-    @board.each do |token|
-      if token == "X" || token == "O"
-        turns_played += 1
-      end
+    @board.count do |token|
+      token == "X" || token == "O"
     end
-    return turns_played
   end
 
   def current_player
-    turns = turn_count
-    if turns % 2 == 0
-      return "X"
-    else
-      return "O"
-    end
+    turn_count.even? ? "X" : "O"
   end
 
   def turn
     puts "Please enter 1-9:"
-
-    user_input = gets.strip
-    index = input_to_index(user_input)
+    display_board
+    input = gets.strip
+    index = input_to_index(input)
     if valid_move?(index)
-     token = current_player
-     move(index, token)
-      else
-        turn
-      end
-      display_board
+      token = current_player
+      move(index, token)
+    else
+      turn
     end
+    display_board
+  end
 
   def won?
-    WIN_COMBINATIONS.each do |win_combination|
-      win_index_1 = win_combination[0]
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
+    WIN_COMBINATIONS.any? do |combo|
+      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+        return combo
+       end
+     end
+   end
 
-      position_1 = @board[win_index_1]
-      position_2 = @board[win_index_2]
-      position_3 = @board[win_index_3]
+   def full?
+     @board.all? {|token| token == "X" || token == "O"}
+   end
 
-      if position_1 == "X" && position_2 == "X" && position_3 == "X" ||
-        position_1 == "O" && position_2 == "O" && position_3 == "O"
-          return win_combination
-        end
-      end
-    else
-      false
-    end
+   def draw?
+     !won? && full?
+   end
 
-    def full?
-      @board.all? do |winner|
-        winner == "X" || winner == "O"
-      end
-    end
+   def over?
+     won? || draw?
+   end
 
-    def draw?
-      full? && !won?
-    end
+   def winner
+     if combo = won?
+       @board[combo[0]]
+     end
+   end
 
+  #  def play
+  #    until over?
+  #      turn
+  #    end
+   #
+  #    if won?
+  #      puts "Congratulations #{winner}!"
+  #    elsif draw?
+  #      puts "Cat's Game!"
+  #    end
+  #
+  #  end
 
-    def over?
-      draw?|| won?
-    end
-
-    def winner
-      if won?
-        return @board[won?[0]]
-      else
-        nil
-      end
-    end
-
-    def play
-      until over?
-        turn
-      end
-      puts "Congratulations #{winner}!" if won?
-      puts "Cat's Game!" if draw?
-    end
+  def play
+    turn until over?
+    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
   end
+
+
+
+end
