20e62349fa68d0cfce6e4492a9f6d74df36f5dcb
Brent Bauman <brentbauman86@gmail.com>
2018-04-08 20:50:57 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,4 @@
-
+#require "pry"
 class TicTacToe
 attr_accessor :board
   def initialize(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
@@ -17,106 +17,96 @@ WIN_COMBINATIONS = [
 ]
 
 def display_board
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
   def input_to_index(string)
+    #binding.pry
     string.to_i - 1
-    index = string.to_i-1
   end
 
 def move(location, token)
-
-    board[location] = token
+  board[location] = token
  end
 
- def position_taken?(board, index)
-   !(board[index].nil? || board[index] == " ")
+ def position_taken?(index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(index)
+   index.between?(0,8) && !position_taken?(index)
  end
 
-def valid_move?(board, index)
-   index.between?(0,8) && !position_taken?(board, index)
- end
-
- def turn(board)
+ def turn
    puts "Please enter 1-9:"
-   input = gets
+   input = gets.strip
    index = input_to_index(input)
-   if valid_move?(board, index)
-    move(board, index, current_player(board))
-    display_board(board)
-   else
-     turn(board)
-  end
+   #binding.pry
+   if valid_move?(index)
+    move(index, current_player)
+    display_board
+else
+turn
+end
  end
 
-def play(board)
- turn = 0
-   until turn == 9
-   turn(board)
-       turn += 1
-   end
- end
-
- def turn_count(board)
+ def turn_count
    count = 0
      board.each do |move|
        if move == "X" || move == "O"
          count += 1
        end
      end
- count
+count
  end
 
- def current_player(board)
-   turn_count(board).even? ? "X" : "O"
+ def current_player
+   turn_count.even? ? "X" : "O"
      end
 
-def won?(board)
-WIN_COMBINATIONS.find do |win_plz|
- board[win_plz[0]] == board[win_plz[1]] && board[win_plz[0]] == board[win_plz[2]] && position_taken?(board, win_plz[1])
+def won?
+WIN_COMBINATIONS.any? do |win_plz|
+ if board[win_plz[0]] == board[win_plz[1]] && board[win_plz[1]] == board[win_plz[2]] && position_taken?(win_plz[1])
+return win_plz
  end
 end
+end
 
-def full?(board)
+
+
+def full?
 board.none? do |filled|
   filled == " " || filled.nil?
   end
  end
 
-def draw?(board)
-full?(board) == true && !won?(board)
+def draw?
+full? && !won?
 end
 
-def over?(board)
-  won?(board) || draw?(board)
+def over?
+  won? || draw?
 end
 
-def winner(board)
-  if won?(board) != nil
-  win_time = board[won?(board)[2]]
+def winner
+  if won?
+  board[won?[0]]
  end
 end
 
-def play(board)
-#binding.pry
-  until over?(board)
-#binding.pry
-    turn(board)
+def play
+until over?
+  turn
   end
-#binding.pry
-  if draw?(board) == true
+
+  if draw? == true
     puts "Cat's Game!"
-  else won?(board)
-    puts "Congratulations #{winner(board)}!"
+ else won?
+    puts "Congratulations #{winner}!"
   end
 end
-
-
-
-
 end
