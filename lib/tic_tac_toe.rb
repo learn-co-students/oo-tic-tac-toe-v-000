391372eebf28710763628837e34a30f645c62199
Dale Terrell <tragicid@gmail.com>
2018-04-30 21:37:05 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,13 +17,13 @@ WIN_COMBINATIONS = [
     @board = [" "," "," "," "," "," "," "," "," "]
   end
   
-    def display_board
-      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-      puts "-----------"
-      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-      puts "-----------"
-      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-    end
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
   
   def move(index, value="X")
     @board[index] = value
