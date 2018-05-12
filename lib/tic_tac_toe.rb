439356a35bec895081d1042d5c2188c3529cd47a
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-05-12 14:11:46 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,9 @@
 class TicTacToe
+  
   def initialize(board)
     @board = board || Array.new(9, " ")
   end
+  
   def WIN_COMBINATIONS
     [
     [0, 1, 2], #top row
@@ -14,12 +16,62 @@ class TicTacToe
     [2, 4, 6]  #top R to bottom L
     ]
   end
-  def display_board(board)
-    @board
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
+  
   def input_to_index(user_input)
-    move = gets.user_input - 1
+    user_input.to_i - 1 #gets user input, subtracts one to coincide with board array index
   end
-  def move(board, index, user_input)
+  
+  def move(board, index, token)
+    
   end
+
+  def position_taken?(index)
+    if index == "X" || index == "O"
+      false
+    end
+  end
+
+  def valid_move?(index)
+    if user_input == (0..9) && position_taken?(index) == false
+      true
+    end
+  end
+  
+  def turn
+    
+  end
+
+  def turn_count
+    @board.count{| token | token == "X" || token == "O"}
+  end
+  
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def won?
+    
+  end
+  
+  def full?(board)
+
+  end
+
+  def draw?(board)
+  end
+
+  def over?(board)
+  end
+  
+  def winner?(board)
+  end
+
 end
