45fb8e327da3afe0c3e579ac636cf9e201c343c8
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-05-15 13:22:39 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,27 +2,13 @@ class TicTacToe
   
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
+    #WIN_COMBINATIONS?
   end
   
-  def WIN_COMBINATIONS
-    [
-    [0, 1, 2], #top row
-    [3, 4, 5], #middle row
-    [6, 7, 8], #bottom row
-    [0, 3, 6], #left column
-    [1, 4, 7], #middle column
-    [2, 5, 8], #right column
-    [0, 4, 8], #top L to bottom R
-    [2, 4, 6]  #top R to bottom L
-    ]
-  end
+  WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
   
   def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]}"
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]}"
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
+    @board
   end
   
   def input_to_index(user_input)
