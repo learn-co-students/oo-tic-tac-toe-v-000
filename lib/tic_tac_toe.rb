83685253fcfcc9f0de20ea522f1258e0659e2710
Brittany Tinnin <bntinnin7@gmail.com>
2018-05-17 19:22:59 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,4 +40,70 @@ class TicTacToe
     !position_taken?(index) && index.between?(0, 8) ? true : false
   end
 
+  def turn_count
+    counter = 0
+    @board.each do |token|
+      if token == "X" || token == "O"
+        counter += 1
+      end
+    end
+    counter
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+      if !valid_move?(index)
+        puts "Whoops! That's not a valid move!"
+        turn
+      end
+    token = current_player
+    move(index, token)
+    display_board
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |win_combo|
+      position_1 = @board[win_combo[0]]
+      position_2 = @board[win_combo[1]]
+      position_3 = @board[win_combo[2]]
+
+      position_1 == "X" && position_2 == "X" && position_3 == "X" ||
+      position_1 == "O" && position_2 == "O" && position_3 == "O"
+   end
+  end
+#
+  def full?
+    @board.all? do |position|
+      position == "X" || position == "O"
+  end
+end
+
+  def draw?
+    !won? && full?
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def winner
+    won? && @board[won?[0]]
+  end
+
+  def play
+    until over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    else
+      puts "Cat's Game!"
+    end
+  end
 end
