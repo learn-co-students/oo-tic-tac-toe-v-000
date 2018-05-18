59818f29e9c637c85c9c20476c68f55e94c88001
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-05-18 15:20:59 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,32 +18,32 @@ end
   def input_to_index(user_input) #DON'T THINK I DID THIS YET
     user_input.to_i - 1 #gets user input, subtracts one to coincide with board array index
   end
-  
-  def move(@board, token = "X")
+ 
+  def move(index, token)
     @board[index] = token
   end
 
-  def position_taken?(@board, index)
-    if @board[index] != "" || @board[index] != " "
-      true
+  def position_taken?(index)
+    if @board[index] == "" || @board[index] == " "
+      false
     end
   end
 
-  def valid_move?(@board, index)
+  def valid_move?(board, index)
     if index.between?(0,8) && !position_taken?(board, index)
       true
     end
   end
   
-  def turn(@board)
+  def turn(board)
     puts "Please enter 1-9"
     user_input = gets.strip
     input_to_index(user_input)
     index = input_to_index(user_input)
-    if valid_move?(@board, index) == true
+    if valid_move?(board, index) == true
       move(board, index, token)
     else
-      turn(@board)
+      turn(board)
     end
   end    
     
@@ -55,52 +55,52 @@ end
     turn_count % 2 == 0 ? "X" : "O"
   end
 
-  def won?(@board)
+  def won?(board)
     WIN_COMBINATIONS.detect do | win_combination |
       @board[win_combination[0]] == @board[win_combination[1]] && @board[win_combination[1]] == @board[win_combination[2]] && position_taken?(@board, win_combintion[0])
     end
   end
   
-  def full?(@board)
+  def full?(board)
     @board.all? {|cell| cell == "X" || cell == "O"}
   end
 
-  def draw?(@board)
-    if !full?(@board) && !won?(@board)
+  def draw?(board)
+    if !full?(board) && !won?(board)
       true
   end
 
-  def over?(@board)
-    if won?(@board) || full?(@board) || draw?(@board)
+  def over?(board)
+    if won?(board) || full?(board) || draw?(board)
       true
     else
       false
     end
   end
   
-  def winner?(@board)
-    if won?(@board)
-      return @board[won?(@board[0])]
+  def winner?(board)
+    if won?(board)
+      return @board[won?(board[0])]
     end
   end
 end
 
-  def play(@board)
+  def play(board)
   turn = 0
   while turn < 9
-    turn(@board)
+    turn(board)
     turn += 1
   end
 end
 
-  def play(@board)
-    until over?(@board)
-      turn(@board)
+  def play(board)
+    until over?(board)
+      turn(board)
     end
-    if won?(@board)
-      winner(@board) == "X" || winner(@board) == "O"
-      puts "Congratulations #{winner(@board)}!"
-    elsif draw?(@board)
+    if won?(board)
+      winner(board) == "X" || winner(board) == "O"
+      puts "Congratulations #{winner(board)}!"
+    elsif draw?(board)
       puts "Cat's Game!"
     end
   end
