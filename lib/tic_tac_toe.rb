28969b74b0d58c0494068c758fb9d5642fd816e0
Jaime Johnsen <drivetraindiva@gmail.com>
2018-05-28 23:53:04 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,8 +1,8 @@
 class TicTacToe 
   
-  def initialize (board = nil)
-    @board = board || Array.new(9," ")
-  end
+  def initialize 
+   @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+     end
 
   WIN_COMBINATIONS = [
   [0,1,2], # Top row
@@ -16,13 +16,15 @@ class TicTacToe
   ]
 end
 
-def display_board
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+
 
 def input_to_index(move)
   index = move.to_i - 1
