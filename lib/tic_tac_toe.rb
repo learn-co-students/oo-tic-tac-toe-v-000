242b671627f01b972563b03b41b767807eb99b49
Vicki Hamm <vicki.cello@gmail.com>
2018-06-14 19:50:17 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,114 @@
+class TicTacToe
+
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(index, current_player = "X")
+  @board[index] = current_player
+  end
+
+  def position_taken?(location)
+    @board[location] != " " && @board[location] != ""
+  end
+
+  def valid_move?(index)
+    index.between?(0, 8) && !position_taken?(index)
+  end
+
+  def turn
+     puts "Please enter 1-9:"
+     input = gets.strip
+     index = input_to_index(input)
+     if valid_move?(index)
+       move(index, current_player)
+       display_board
+   else
+     turn
+   end
+end
+
+  def current_player
+  turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def turn_count
+  @board.count{|token| token == "X" || token == "O"}
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |combination|
+      if combination.all? {|i| @board[i] == "X"} || combination.all? {|i| @board[i] == "O"}
+        return combination
+      end
+    end
+    return false
+  end
+
+  def full?
+    @board.all? {|space| space == "X" || space == "O"}
+  end
+
+  def draw?
+    if full? && !won?
+      return true
+    else
+      return false
+    end
+  end
+
+  def over?
+    if won? || draw? || full?
+      return true
+    else return false
+    end
+  end
+
+  def winner
+    if won?
+    winning_combination = won?
+    if winning_combination.all? {|i| @board[i] == "X"}
+        return "X"
+     elsif winning_combination.all? {|i| @board[i] == "O"}
+        return "O"
+     end
+   else
+     return nil
+  end
+end
+
+ def play
+  until over?
+  turn
+ end
+  if won?
+    puts "Congratulations #{winner}!"
+  else
+    puts "Cat's Game!"
+  end
+end
+
+end
