c24595fa4e7c2a540c9287044f3ff90056faa242
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-23 04:16:32 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,6 +61,15 @@ class TicTacToe
       end
     end
   
+  def turn_count
+    counter = 0
+    @board.each do |token|
+      if token == "X" || token == "O"
+        counter += 1
+      end
+    end
+    return counter
+  end
   
 end
  
