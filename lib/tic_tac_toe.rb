9234577670d6a65d605fdf0f04704c0a7910466f
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-23 04:12:14 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,8 +48,19 @@ class TicTacToe
       false 
     end
     end
-    
-
-
+  
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, token="X")
+      display_board
+    else
+        turn
+      end
+    end
+  
+  
 end
  
