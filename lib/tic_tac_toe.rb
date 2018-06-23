f71f83ea9bf5fd2d3e16d7132f27158edebf5dd3
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-23 03:34:43 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,33 +1,27 @@
 class TicTacToe
-  def initialize(board=[" ", " ", " ", " ", " ", " ", " ", " ", " "])
-     @board = board
+  
+  def initialize()
+     @board = Array.new(9, " ")
    end
-   WIN_COMBINATIONS = [
-     [0, 1, 2],
-     [3, 4, 5],
-     [6, 7, 8],
-     [0, 3, 6],
-     [1, 4, 7],
-     [2, 5, 8],
-     [0, 4, 8],
-     [2, 4, 6]
-     ]
- end
- 
- class  DisplayBoard 
-   def initialize(board)
-     @board = board 
-  end
-   def board
-   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+
+  WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8], 
+  [6, 4, 2],
+  [1, 4, 7],
+  [0, 3, 6],
+  [2, 5, 8]
+  ]
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     separator = "-----------"
     puts separator
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts separator
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
-   end
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 end
-   
-   
-   
-   
+
