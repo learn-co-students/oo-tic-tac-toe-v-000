24566a4d5962900b3cdc870f7b14ec2e02d97a2e
Dana Palmer <roiai4@hotmail.com>
2018-06-29 22:11:33 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,26 +1,36 @@
+require "pry"
 class TicTacToe
-  def initialize
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
   end
-
-  WIN_COMBINATIONS = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]
-  ]
-
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  
+  def board
+    @board
   end
-
+    
+    WIN_COMBINATIONS = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+      [0, 4, 8]
+      ]
+    
+    def display_board
+      puts "  #{@board[0]} | #{@board[1]} | #{@board[2]} "
+      puts " ----------- "
+      puts "  #{@board[3]} | #{@board[4]} | #{@board[5]} "
+      puts " ----------- "
+      puts "  #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+  
   def input_to_index(user_input)
     user_input.to_i - 1
   end
-
+  
   def move(index, token)
     @board[index] = token
   end
