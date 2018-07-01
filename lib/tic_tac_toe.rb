00c6988c0e5045231f0aea1ddd7129d001c470fe
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-01 01:35:43 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,115 @@
+class TicTacToe
+  
+def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+    
+    end
+    
+    WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+     [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(input) 
+    index = (input.to_i) - 1
+  end 
+  
+  def move(index, token = "X")
+  @board[index] = token
+  end 
+
+def position_taken?(index)
+if @board[index] == " "|| @board[index] == ""|| @board[index] == nil
+  return false
+else
+  return true
+end
+end
+
+def valid_move?(index)
+  if position_taken?(index) == false && index.between?(0,8)
+    return true
+  else
+    return false
+  end
+end
+
+def turn
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+
+if valid_move?(index)
+  move(index, current_player)
+  display_board
+else
+  turn
+end
+end
+
+
+def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+  
+def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+  
+  def won?
+  WIN_COMBINATIONS.detect do |win_combos|
+       @board[win_combos[0]] == @board[win_combos[1]] && @board[win_combos[1]] == @board[win_combos[2]] && position_taken?( win_combos[0])
+end
+  end
+  
+  def full?
+WIN_COMBINATIONS.each do |elements|
+    if !position_taken?(elements[0])
+      return false
+end
+end
+end
+
+
+def draw?
+  !won? && full?
+end
+
+def over?
+  if won? || draw?||full?
+    return true
+end
+end
+
+def winner
+if token = won?
+ return @board[token[0]]
+end
+end
+
+def play
+
+until over?
+  turn
+end
+if draw?
+  puts "Cat's Game!"
+elsif won? && winner == "O"
+  puts "Congratulations O!"
+elsif won? && winner == "X"
+  puts "Congratulations X!"
+
+
+end
+end
+
+end
+
+
+
