e3515dd4c75dd132da1fd60ef60a4db62b44153e
Rafael Lopez <lpz.raf@gmail.com>
2018-07-10 03:24:09 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,56 @@
+class TicTacToe
+  # assign instance var to @board
+   def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+    end
+# win combination
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+  
+# display_board 
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
+# input_to_index 
+def input_to_index(input)
+   input.to_i - 1
+end
+
+# move
+def move(board, index, current_player)
+   board[index] = current_player
+end
+
+# valid_move? 
+def valid_move?(board, index)
+  if index < 0  || index > 8
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return false
+  else
+    true
+  end
+end
+
+# position_taken? 
+def position_taken?(board, index)
+    if board[index] == " "
+    return false
+    elsif board[index] == "X" || board[index] == "O" 
+    return true 
+ end
+end
+end
