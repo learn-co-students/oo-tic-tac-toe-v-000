96d8e31c3e3c61e2bbf045ca1cbdc212b2430281
Susan Meyers <smmeyers24@gmail.com>
2018-07-25 17:39:12 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,11 @@ class TicTacToe
       index = input_to_index(input)
 
     if valid_move?(index)
-      move(index)
+      move(index, character = current_player)
+      display_board
+    else
+      puts "invalid move"
+      turn
    end
    end
 
@@ -75,13 +79,116 @@ class TicTacToe
 
 
 
+  def current_player
+    if turn_count % 2 == 0
+     "X"
+   else
+     "O"
+    end
+  end
 
-  #def current_player(board)
-    #if turn_count % 2 == 0
-    #{}"X"
-    #end
 
+
+  def won?
+
+  WIN_COMBINATIONS.each do |win_combo|
+    win_index_1 = win_combo[0]
+    win_index_2 = win_combo[1]
+    win_index_3 = win_combo[2]
+
+    position_1 = @board[win_index_1] # load the value of the board at win_index_1
+    position_2 = @board[win_index_2] # load the value of the board at win_index_2
+    position_3 = @board[win_index_3]
+  # load the value of the board at win_index_3
+   if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return win_combo
+   end
+   end
+    return false
+   end
+
+
+  def full?
+   @board.all? {|space| space != " " }
+  #board.all?{|token| token == "X" || token == "O"}
+ end
+
+
+ def draw?
+   if full? && !won?
+    return true
+   if !full? || !won?
+    return false
+   end
+   end
+ end
+
+
+
+ def over?
+  if won? || draw? || full?
+    return true
+  else
+    return false
+  end
+  end
+
+
+  def winner
+    win_combo = won?
+
+    if win_combo != false
+      if @board[win_combo[0]] == "X"
+        return "X"
+      else
+        return "O"
+      end
+    else
+      return nil
+    end
+
+
+
+  def play
+    turn
+    if valid_move?
+    move
+    while !over?
+    turn
+    end
+  end
 end
+
+
+
+
+
+#   if won?
+#     puts "Congratulations #{winner(board)}!"
+#    else
+#     puts "Cat's Game!"
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
