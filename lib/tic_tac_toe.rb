ab6c4bf6a62e3a411bcb6d16a2b4e0d25bb315c8
beatricemendoza01 <39242463+beatricemendoza01@users.noreply.github.com>
2018-07-30 15:36:14 -0400

    Removing more unnecessary parenthesis

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,8 +43,8 @@ class TicTacToe
     input = gets.strip
     index = input_to_index(input)
     if valid_move?(index)
-      move(index, current_player())
-      display_board()
+      move(index, current_player)
+      display_board
     else
       turn
     end
