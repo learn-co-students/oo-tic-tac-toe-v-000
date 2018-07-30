796b9d69cbc08d8dafcd04e982f431b7283b0018
beatricemendoza01 <39242463+beatricemendoza01@users.noreply.github.com>
2018-07-30 15:34:35 -0400

    Remove unnecessary parenthesis
    Remove unnecessary parenthesis for method definitions and method calls that do not have arguments.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,7 +14,7 @@ class TicTacToe
     [2, 4, 6]
   ]
 
-  def display_board()
+  def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -38,7 +38,7 @@ class TicTacToe
     !position_taken?(index) && index.between?(0, 8)
   end
 
-  def turn()
+  def turn
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
@@ -46,11 +46,11 @@ class TicTacToe
       move(index, current_player())
       display_board()
     else
-      turn()
+      turn
     end
   end
 
-  def turn_count()
+  def turn_count
     counter = 0
     @board.each do |board_space|
       if board_space == "X" || board_space == "O"
@@ -60,11 +60,11 @@ class TicTacToe
     counter
   end
 
-  def current_player()
-    turn_count() % 2 == 0 ? "X" : "O"
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
   end
 
-  def won?()
+  def won?
     WIN_COMBINATIONS.detect do |win_combination|
       if @board[win_combination[0]] == @board[win_combination[1]] && @board[win_combination[1]] == @board[win_combination[2]] && position_taken?(win_combination[0]) && position_taken?(win_combination[1]) && position_taken?(win_combination[2])
         return win_combination
@@ -73,27 +73,27 @@ class TicTacToe
     return false
   end
 
-  def full?()
+  def full?
     @board.all? {|board_space| board_space == "X" || board_space == "O"}
   end
 
-  def draw?()
-    !won?() && full?()
+  def draw?
+    !won? && full?
   end
 
-  def over?()
-    won?() || draw?()
+  def over?
+    won? || draw?
   end
 
-  def winner()
-    win_combination = won?()
+  def winner
+    win_combination = won?
     win_combination.class == Array ? @board[win_combination[0]] : nil
   end
 
-  def play()
-    until over?()
-      turn()
+  def play
+    until over?
+      turn
     end
-    won?() ? (puts "Congratulations #{winner()}!") : (puts "Cat's Game!")
+    won? ? (puts "Congratulations #{winner}!") : (puts "Cat's Game!")
   end
 end
