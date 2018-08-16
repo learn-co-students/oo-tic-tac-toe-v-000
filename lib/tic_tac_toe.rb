7533a5f42fbe8d31f09f83c3a6831799bebf845e
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-16 19:07:54 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,89 +27,85 @@ WIN_COMBINATIONS = [
   end
   
     def move(index, value)
-    @board[index] = value
+      @board[index] = value
+    end
+  
+    def position_taken?(location)
+      @board[location] != " " && @board[location] != ""
+    end
+
+    def valid_move?(index)
+      index.between?(0,8) && !position_taken?(index)
+    end
+  
+    def turn
+      puts "Please enter 1-9:"
+      input = gets.strip 
+      index = input_to_index(input)
+    if valid_move?(index)
+      move(index, value = current_player())
+      display_board
+    else
+      turn
+    end     
+    end
+  
+  def turn_count
+    counter = 0
+    @board.each do |space|
+      if space != " "
+        counter += 1
+      end
+    end
+    counter
   end
   
-  def position_taken?(location)
-    @board[location] != " " && @board[location] != ""
-  end
-
-def valid_move?(index)
-  index.between?(0,8) && !position_taken?(index)
-end
-
-def turn
-  puts "Please enter 1-9:"
-  input = gets.strip 
-  index = input_to_index(input)
-  if valid_move?(index)
-    move(index, value = current_player())
-    display_board
-  else
-    turn
-  end     
-end
-
-def turn_count
-  counter = 0
-  @board.each do |space|
-    if space != " "
-      counter += 1
+  def current_player
+    if turn_count % 2 == 0 
+      return "X"
+    else
+      return "O"
+  
     end
   end
-  counter
-end
-
-def current_player
-  if turn_count % 2 == 0 
-    return "X"
-  else
-    return "O"
-
-  end
-end
-
-def won?
-  WIN_COMBINATIONS.detect do |combo|
-    @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]] && position_taken?(combo[0])
-  end
-end
-
-def full?(board)
-  board.all? do |space|
-    space != " " 
-  end
-end
-
-def draw?(board)
-  !won?(board) && full?(board)
- end
-
-def over?(board)
- draw?(board) || won?(board) || full?(board) 
- 
-end
-
-def winner(board)
-  if win_combo = won?(board)
-    board[win_combo.first] 
-
-end
-end
-
-def play(board)
- 
-  turn(board)
-  if over?(board)
-    return winner(board) 
-else
- counter = 0 
-  until counter == 9 
-  play(board)
-counter += 1 
-end
   
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]] && position_taken?(combo[0])
+    end
+  end
+  
+  def full?
+    @board.all? do |space|
+      space != " " 
+    end
+  end
+  
+  def draw?
+    !won? && full?
+   end
+  
+  def over?
+   draw? || won? || full? 
+  end
+  
+  def winner
+    if win_combo = won?
+      @board[win_combo.first] 
+  
+  end
+  end
+  
+  def play
+    while !over?
+        turn
+    end
+    if won?
+      puts "Congratulations #{winner}!" 
+    else
+    puts "Cat's Game!"
+    
+  end
 end
 end
-
 
