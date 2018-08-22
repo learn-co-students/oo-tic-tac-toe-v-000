e8df0b251d5ca1d566ae5b153656997064ef9b03
Ashley Wong <ashwong04@gmail.com>
2018-08-22 15:30:55 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,11 +32,31 @@ class TicTacToe
   end
   
   def turn_count
-
-
+    counter = 0
+    @board.each do |count|
+      if count == "O" || count == "X"
+      counter += 1
+      end
+    end
+    return counter
   end
-
-
+  
+  
+  
+#def current_player(board)
+#  if turn_count(board) % 2 == 0
+ #   return "X"
+#  elsif turn_count(board) % 2 != 0
+#    return "O"
+#  end
+#end
+  
+  
+  
+  
+ # def turn
+   # puts "Please enter 1-9:"
+  #end
 
 
 end
