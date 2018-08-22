bc43f2a051140d7dd1280e01817998243c305e16
Ashley Wong <ashwong04@gmail.com>
2018-08-22 19:21:11 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,6 +62,23 @@ class TicTacToe
     end
   end
 
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+    position_1 = @board[win_index_1]
+    position_2 = @board[win_index_2]
+    position_3 = @board[win_index_3]
+    
+      if position_taken?(@board, win_index_1) && position_2 == position_1 && position_3 == position_2
+         return win_combination
+      else return false
+      end     
+    end
+  end
+
 
 end
 
