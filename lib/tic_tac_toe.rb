a0f361acdd0fc0f76e385ad175296e10137b1978
Stef Claus <stef.claus@gmail.com>
2018-08-31 20:45:18 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -108,4 +108,33 @@ def draw?
   end 
 end #ends draw?
 
+def over?
+  if won?
+    true 
+  elsif draw?
+    true
+  end #ends if
+end #ends over 
+
+def winner
+  if won?
+    true
+    WIN_COMBINATIONS.each do |win_combination|
+     win_index_1 = win_combination[0]
+     win_index_2 = win_combination[1]
+     win_index_3 = win_combination[2]
+    
+     position_1 = board[win_index_1]
+     position_2 = board[win_index_2] 
+     position_3 = board[win_index_3]
+     
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return "X" 
+      elsif position_1 == "O"  && position_2 == "O" && position_3 == "O"
+      return "O" 
+      end #end if 
+    end #end each/do block
+  end #end if statement 
+end #end win_combinations
+
 end 
