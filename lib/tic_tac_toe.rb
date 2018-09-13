4ad0bffbd29c8fb305070175da61d324c1250ed2
veloblank <veloblank@gmail.com>
2018-09-13 08:32:00 -0400

    Redoing lab. Refactoring.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,98 +1,115 @@
 require 'pry'
-
 class TicTacToe
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
   def initialize
     @board = Array.new(9, " ")
   end
 
-WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
-def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
+  def input_to_index(input)
+    input.to_i - 1
+  end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
+  def move(index, token)
+    @board[index] = token
+  end
 
-def move(index, token)
-  @board[index] = token
-end
+  def position_taken?(index)
+    if @board[index] == "X" || @board[index] == "O"
+      true
+    else
+      false
+    end
+  end
 
-def position_taken?(index)
-  @board[index] != " "
-end
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
 
-def valid_move?(index)
-  index.between?(0,8) && !position_taken?(index)
+  def turn_count
+    counter = 0
+    @board.each do |cell|
+      if cell == "X" || cell == "O"
+        counter += 1
+      end
+    end
+  counter
+  end
+
+
+def current_player
+  if turn_count.even?
+    "X"
+  else
+    "O"
+  end
 end
 
 def turn
   puts "Please enter 1-9:"
-  input = gets.strip
+  input = gets.chomp
   index = input_to_index(input)
   if valid_move?(index)
     token = current_player
-    move(index, token)
   else
     turn
   end
+  move(index, token)
   display_board
+
+
+
 end
 
-def turn_count
-  @board.count{|turns_played| turns_played != " "}
+def won?
+  #returns first matching array in WIN_COMBINATIONS where ["X","X","X"] or ["O","O","O"]
+  #the winning marker of the game would simply be any element from the winner variable: winner.first
+  #method is thinking a combo of [" ", " ", " "] is a winner
+  WIN_COMBINATIONS.detect do |var|
+    if @board[var[0]] == @board[var[1]] &&
+       @board[var[1]] == @board[var[2]]
+    return var
+    end
+  end
 end
 
-  def current_player
-    if turn_count.even?
-      "X"
-    else
-      "O"
-    end
-  end
+def draw?
 
-  def won?
-    WIN_COMBINATIONS.detect do |var|
-      if @board[var[0]] == @board[var[1]] &&  @board[var[1]] == @board[var[2]] && position_taken?(var[0])
-        return var
-      end
-    end
-  end
+end
 
-  def full?
-    @board.all?{|marker| marker == "X" || marker == "O"}
-  end
+def full?
 
-  def draw?
-    !won? && full?
-  end
+end
 
-  def over?
-    draw? || won?
-  end
+def over?
 
-  def winner
-    if winner_chicken_dinner = won?
-      @board[winner_chicken_dinner.first]
-    end
-  end
 
-  def play
-    turn until over?
-    puts winner ? "Congratulations #{winner}!" : "Cat's Game!" 
+end
+
+def winner
+  if winning_combination = won? #method is thinking a combo of [" ", " ", " "] is a winner
+    binding.pry
+    @board[winning_combination.first]
   end
+end
+
+
+
 end
