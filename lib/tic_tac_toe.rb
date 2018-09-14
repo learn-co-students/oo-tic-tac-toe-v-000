a742defecefef8685c80c3d45120c09e817cea8d
Drake Rentrop <drakerentrop@gmail.com>
2018-09-14 22:15:27 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,15 +12,6 @@ class TicTacToe
   [0,4,8], # Digonal top left to bottom right
   [2,4,6]  # Digonal top right to bottom left
 ]
-
-  def current_player
-    turn_count % 2 == 0 ? "X" : "O"
-  end
- 
-  def turn_count
-    @board.count{|token| token == "X" || token == "O"}
-  end
- 
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -28,4 +19,81 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+def input_to_index(input)
+  input.to_i - 1
+end
+  def move(index, token)
+  @board[index.to_i - 1] = token
+end
+def position_taken?(board, index)
+ board[index] !=  " " 
+end
+def valid_move?(board, index)
+       index.between?(0, 8) && !position_taken?(board, index)
+  end
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board,index)
+    player_move(board, index, current_player(board))
+    display_board(board)
+   else
+    puts "try again"
+    turn(board)
+  end
+  display_board(board)
+end
+
+ def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+ end
+ 
+ def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+ end
+ def won?(board)
+  WIN_COMBINATIONS.each {|win_combination|
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+  
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
+   if position_1 == "X" && position_2 == "X" && position_3 == "X"
+     return win_combination
+   elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+   return win_combination
+ end
+  }
+  false 
+  
+end
+def full?(board)
+  board.all? {|index| index == "X" || index =="O"}
+end
+def draw?(board)
+  if !won?(board) && full?(board)
+    true 
+  else
+    false 
+  end
+end
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    true
+  else
+    false
+  end
+end
+def winner(board)
+  index = won?(board)
+  if index 
+    return @board[index[0]]
+  end
+end
+
+
+
 end
