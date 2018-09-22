575af1c0442b6f1a19e02d2acf908f627006ce88
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-09-22 12:52:56 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,11 +3,11 @@ class TicTacToe
 	    @board = board || Array.new(9, " ")
 	  end
 def display_board(board)
-  puts " #{ @board[0] } | #{ @board[1] } | #{ @board[2] } "
+  puts " #{ board[0] } | #{ board[1] } | #{ board[2] } "
   puts "-----------"
-  puts " #{ @board[3] } | #{ @board[4] } | #{ @board[5] } "
+  puts " #{ board[3] } | #{ board[4] } | #{ board[5] } "
   puts "-----------"
-  puts " #{ @board[6] } | #{ @board[7] } | #{ @board[8] } "
+  puts " #{ board[6] } | #{ board[7] } | #{ board[8] } "
 end
 
 def input_to_index(user_input)
@@ -126,6 +126,5 @@ def play(board)
   elsif draw?(board)
     puts "Cat's Game!"
   end
-end
 end
 end
