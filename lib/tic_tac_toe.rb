038144c4fe4cd36da4933a4e8c3c6c0c823aa57c
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-09-22 13:16:28 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,7 @@ class TicTacToe
   def initialize(board = nil)
 	    @board = board || Array.new(9, " ")
 	  end
-def display_board(board)
+def display_board
   puts " #{ @board[0] } | #{ @board[1] } | #{ @board[2] } "
   puts "-----------"
   puts " #{ @board[3] } | #{ @board[4] } | #{ @board[5] } "
