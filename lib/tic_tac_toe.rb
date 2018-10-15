2870474808201bef97980560570237e7dc3b37e2
Lois Lee <lois.lee@me.com>
2018-10-15 23:04:35 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,53 +12,53 @@ class TicTacToe
     @board.count{|token| token == "X" || token == "O"}
   end
 
-def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
-def input_to_index(input)
-  input.to_i - 1
-end
+  def input_to_index(input)
+    input.to_i - 1
+  end
 
-def move(index,current_player="X")
+  def move(index,current_player="X")
     @board[index] = current_player
   end
 
-def position_taken?(index)
+  def position_taken?(index)
    if @board[index] == " " || @board[index] == "" || @board[index] == nil
        false
      else @board[index] == "X" || @board[index] == "O"
        true
-   end
-   end
+  end
+  end
 
-   def valid_move?(index)
-     if index.between?(0,8) && !position_taken?(index)
-       true
-     else
-       false
+  def valid_move?(index)
+   if index.between?(0,8) && !position_taken?(index)
+     true
+   else
+     false
    end
-   end
+  end
 
-WIN_COMBINATIONS =[
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,3,6],
-[1,4,7],
-[2,5,8],
-[0,4,8],
-[6,4,2]
-]
+  WIN_COMBINATIONS =[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+  ]
 
-def won?
-    WIN_COMBINATIONS.detect do |win_combination|
-(@board[win_combination[0]] == "X" && @board[win_combination[1]] == "X" && @board[win_combination[2]] == "X") || (@board[win_combination[0]] == "O" && @board[win_combination[1]] == "O" && @board[win_combination[2]] == "O")
-end
+  def won?
+      WIN_COMBINATIONS.detect do |win_combination|
+  (@board[win_combination[0]] == "X" && @board[win_combination[1]] == "X" && @board[win_combination[2]] == "X") || (@board[win_combination[0]] == "O" && @board[win_combination[1]] == "O" && @board[win_combination[2]] == "O")
+  end
 end
 
 
