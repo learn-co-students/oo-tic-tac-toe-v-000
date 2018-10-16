0e3f73ad3086f8a4cefba9d1bdb0beb0fea9bd29
Angie Goza <angielg9888@gmail.com>
2018-10-16 04:37:54 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,156 @@
 class TicTacToe
-  def initialize(board)
-    @board = board || [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
+  
+  WIN_COMBINATIONS = [[0,1,2],
+                    [3,4,5],
+                    [6,7,8],
+                    [0,3,6],
+                    [1,4,7],
+                    [2,5,8],
+                    [0,4,8],
+                    [2,4,6]
+                   ]
+  
+  def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} " 
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(index, character)
+  @board[index] = character
+end
+
+def position_taken?(index)
+  if @board[index] == " " || @board[index] == "" || @board[index] == nil
+     return false
+  else 
+     return true
+    end
+  end
+  
+def valid_move? (index)
+  if index.between?(0, 8)
+      if @board[index] == " " || @board[index] == "" || @board[index] == nil
+        return true
+      else 
+        return false
+      end
+  else
+    return false
+  end
+end
+
+def turn_count
+  counter = 0
+  @board.each do |index|
+    if index == "X" || index == "O"
+      counter += 1
+    end
+  end
+    return counter
+  end
+  
+def current_player
+  turn_count % 2 == 0 ? "X" : "O"
+end
+
+def turn
+  puts "Please enter 1-9:"
+  input = gets.strip
+  user_input = input_to_index(input)
+  if valid_move?(user_input) == false
+    turn
+  else move(user_input, character = current_player)
+  end
+  display_board
+end
+
+def won?
+  WIN_COMBINATIONS.each do |win_combination|
+      # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+      # grab each index from the win_combination that composes a win.
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      
+      
+      position_1 = @board[win_index_1] # load the value of the board at win_index_1
+      position_2 = @board[win_index_2] # load the value of the board at win_index_2
+      position_3 = @board[win_index_3] # load the value of the board at win_index_3
+      
+        
+        if position_1 == "X" && position_2 == "X" && position_3 == "X"
+          return win_combination # return the win_combination indexes that won.
+        elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+          return win_combination
+        end
+  end
+  false
+end
+
+def full?
+  @board.all? {|space| space != " "}
+end
+
+def draw?
+  if full? == true
+    if won? == false
+      true
+    else
+      false
+    end
+  else 
+    false
+  end
+end
+
+def over?
+ won? || draw?
+end
+
+def winner
+  WIN_COMBINATIONS.each do |win_combination|
+      # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+      # grab each index from the win_combination that composes a win.
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      
+      
+      position_1 = @board[win_index_1] # load the value of the board at win_index_1
+      position_2 = @board[win_index_2] # load the value of the board at win_index_2
+      position_3 = @board[win_index_3] # load the value of the board at win_index_3
+      
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return "X"
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return "O"
+      end
+  end
+  nil
+end
+
+  def play
+    until over?
+      turn
+    end
+    
+    if winner == "X"
+     puts "Congratulations X!"
+   elsif winner == "O"
+     puts "Congratulations O!"
+   elsif draw? == true
+     puts "Cat's Game!"
+  end
+end
 end
