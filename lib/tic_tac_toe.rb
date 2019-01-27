41beb905a9f436bb8334d254bf2cf7c848cebee3
Melissa Hummelt <mkhummelt@gmail.com>
2019-01-27 20:21:28 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,21 +1,20 @@
 class TicTacToe
-
-  def initialize(board = nil)
-    @board = board || Array.new(9, " ")
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
 
-   WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
   ]
 
-   def display_board
+  def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -23,20 +22,20 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-   def input_to_index(input)
-    input.to_i - 1
+  def input_to_index(user_input)
+    user_input.to_i - 1
   end
 
-   def move(index, token = "X")
+  def move(index, token)
     @board[index] = token
   end
 
-   def position_taken?(index)
-    @board[index] == "X" || @board[index] == "O"
+  def position_taken?(index)
+    @board[index] != " "
   end
 
-   def valid_move?(index)
-    !(position_taken?(index)) && index.between?(0,8)
+  def valid_move?(index)
+    !position_taken?(index) && index.between?(0,8)
   end
 
    def turn_count
